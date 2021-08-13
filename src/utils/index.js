4c7937450abaee5c1131b93fd8f640acf7bd67bf
Egi A Wibowo <egiwibowo13@gmail.com>
2021-08-13 10:06:07 +0700

    create service songs

---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+const mapDBToSongsModel = (db) => ({
+  id: db?.id,
+  title: db?.title,
+  performer: db?.performer,
+});
+
+const mapDBToSongModel = (db) => ({
+  id: db?.id,
+  title: db?.title,
+  performer: db?.performer,
+  year: db?.year,
+  genre: db?.genre,
+  duration: db?.duration,
+  insertedAt: db?.insertedAt,
+  updatedAt: db?.updatedAt,
+});
+
+module.exports = { mapDBToSongsModel, mapDBToSongModel };
