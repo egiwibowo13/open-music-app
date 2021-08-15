a677eecdd8f73c1fa2d809172fbd7ccd3503e071
Egi A Wibowo <egiwibowo13@gmail.com>
2021-08-16 04:59:15 +0700

    add onPreResponse sample

--- src/server.js
+++ src/server.js
@@ -27,6 +27,33 @@ const init = async () => {
     },
   });
 
+  // server.ext('onPreResponse', (request, h) => {
+  //   // mendapatkan konteks response dari request
+  //   const { response } = request;
+  //   if (response instanceof ClientError) {
+  //     // membuat response baru dari response toolkit sesuai kebutuhan error handling
+  //     const newResponse = h.response({
+  //       status: 'fail',
+  //       message: response.message,
+  //     });
+  //     newResponse.code(response.statusCode);
+  //     return newResponse;
+  //   } if (response instanceof Error) {
+  //     const newResponse = h.response({
+  //       status: 'error',
+  //       message: 'Maaf, terjadi kegagalan pada server kami.',
+  //     });
+  //     newResponse.code(500);
+  //     // console.error(error);
+  //     return newResponse;
+  //   }
+
+  //   // Server ERROR!
+
+  //   // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa terintervensi)
+  //   return response.continue || response;
+  // });
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
