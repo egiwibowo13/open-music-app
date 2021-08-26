820af11ef8e708253f36b6acd4e0f0754b265f70
Egi A Wibowo <egiwibowo13@gmail.com>
2021-08-26 20:16:19 +0700

    implement cache service when songs playlist

--- src/services/postgres/PlaylistSongsService.js
+++ src/services/postgres/PlaylistSongsService.js
@@ -3,8 +3,10 @@ const { nanoid } = require('nanoid');
 const InvariantError = require('../../exceptions/InvariantError');
 
 class PlaylistSongsService {
-  constructor() {
+  constructor(cacheService) {
     this._pool = new Pool();
+
+    this._cacheService = cacheService;
   }
 
   async addPlaylistSong(playlistId, songId) {
@@ -18,19 +20,27 @@ class PlaylistSongsService {
     if (!result.rowCount) {
       throw new InvariantError('Lagu gagal ditambahkan ke playlist');
     }
+    await this._cacheService.delete(`playlistsongs:${playlistId}`);
     return result.rows[0].id;
   }
 
   async getPlaylistSongs(playlistId) {
-    const query = {
-      text: `SELECT songs.id, songs.title, songs.performer FROM songs 
-            INNER JOIN playlistsongs ON playlistsongs.song_id = songs.id
-            WHERE playlistsongs.playlist_id = $1
-            GROUP BY songs.id`,
-      values: [playlistId],
-    };
-    const result = await this._pool.query(query);
-    return result.rows;
+    try {
+      const result = await this._cacheService.get(`playlistsongs:${playlistId}`);
+      return JSON.parse(result);
+    } catch (error) {
+      const query = {
+        text: `SELECT songs.id, songs.title, songs.performer FROM songs 
+              INNER JOIN playlistsongs ON playlistsongs.song_id = songs.id
+              WHERE playlistsongs.playlist_id = $1
+              GROUP BY songs.id`,
+        values: [playlistId],
+      };
+      const result = await this._pool.query(query);
+      // catatan akan disimpan pada cache sebelum fungsi getNotes dikembalikan
+      await this._cacheService.set(`playlistsongs:${playlistId}`, JSON.stringify(result.rows));
+      return result.rows;
+    }
   }
 
   async deletePlaylistSong(playlistId, songId) {
@@ -43,6 +53,7 @@ class PlaylistSongsService {
     if (!result.rowCount) {
       throw new InvariantError('Playlistsong gagal dihapus');
     }
+    await this._cacheService.delete(`playlistsongs:${playlistId}`);
   }
 
   async verifyPlaylistSong(playlistId, songId) {
